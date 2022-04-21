4d42d866b4e22fb7edcfca12e8d6d6a55f250378
Alexey-Kokushkin <kokushkin_83@mail.ru>
2022-04-21 21:03:40 +0900

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Проверка на простое число:
+//N = 13 -> "Это простое число"
+//N = 12 -> “Это не простое число”
+
+Console.WriteLine("Введите число N: ");
+String inputValue = Console.ReadLine();
+int N = int.Parse(inputValue);
+int i=1;
+int quantity = 0;
+while ( i<= N)
+{
+    if (N%i==0)
+    {
+        quantity+=1;
+    }
+    i+=1;
+}
+Console.WriteLine($"{quantity} => количество чисел - делителей без остатка для заданного числа {N}");
+Console.WriteLine();
+if (quantity>2)
+{
+    Console.WriteLine($"{N} не является простым числом ");
+}
+else Console.WriteLine($"{N} является простым числом ");
